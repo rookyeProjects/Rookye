cae7cfd2e517e9a70ef7c7b34575c19811bdafa2
Eibar <ing.ponce@rookye.com.mx>
2018-06-29 09:38:28 -0500

    .

--- app/Iniciador.php
+++ app/Iniciador.php
@@ -3,9 +3,13 @@
 	//Cargamos librerias
 	require_once 'config/Configurar.php';
 
+
+	//require_once 'librerias/Base.php';
+	//require_once 'librerias/Controlador.php';
+	//require_once 'librerias/Core.php';
+
+	//AutoLoad php
+
 	spl_autoload_register(function($nombreClase){
 		require_once 'librerias/' . $nombreClase . '.php';
 	});
-	$controlador = new Controlador($_GET["controlador"], $_POST);
-
-	
